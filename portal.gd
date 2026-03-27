0483e099ba0556fb001a7b68f1c750836d0d2a5e
Josh Rouleau <p5t3b@unb.ca>
2026-03-27 15:36:32 -0300

    add portal sprite, change name of main to test

--- portal.gd
+++ portal.gd
@@ -2,4 +2,5 @@ extends Area2D
 @export var nextLevel : String #put filepath of next level here
 
 func _on_body_entered(body):
-	get_tree().change_scene_to_file(nextLevel) 
+	if get_tree().get_nodes_in_group("player").has(body):
+		get_tree().change_scene_to_file(nextLevel) 
