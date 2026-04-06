bde0dad6d0219049ef57fa94c702bb9c429cfaab
Rouleaus <joshuapmrouleau@gmail.com>
2026-04-06 14:19:44 -0300

    add transition effect

--- portal.gd
+++ portal.gd
@@ -1,6 +1,10 @@
 extends Area2D
 @export var nextLevel : String #put filepath of next level here
 
-func _on_body_entered(body):
-	if get_tree().get_nodes_in_group("player").has(body):
-		get_tree().change_scene_to_file(nextLevel) 
+#func _on_body_entered(body):
+	#if get_tree().get_nodes_in_group("player").has(body):
+		#$ColorRect.transition_to(nextLevel)
+
+
+#func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	#$ColorRect.transition_to(nextLevel)
